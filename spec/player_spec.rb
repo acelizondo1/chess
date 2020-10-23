5f76be4293344e8159459505ca28ead1852ff2f4
Aaron <acelizondo11@gmail.com>
2020-10-22 22:15:43 -0500

    Implemented load_start_pieces in Player class with testing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,15 @@
+require './lib/player.rb'
+
 describe Player do
   let(:white_player) do
     @white_player = Player.new("white")
+  end
+
+  describe "#initialize" do
+    it "creates an array of starting pieces storing them in active_pieces" do
+      expect(white_player.active_pieces[0]).to be_kind_of(Rook)
+      expect(white_player.color).to eql("white")
+      expect(white_player.active_pieces[3]).to be_kind_of(Queen)
+    end
   end
 end
