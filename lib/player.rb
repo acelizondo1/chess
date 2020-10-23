0ecaaec68604bfa09d9fe0436a8f027b2aae1a6a
Aaron <acelizondo11@gmail.com>
2020-10-22 22:46:09 -0500

    adjusted data structure for active_pieces

--- lib/player.rb
+++ lib/player.rb
@@ -8,25 +8,23 @@ attr_reader :color, :active_pieces, :eliminated_pieces
 
   private
   def load_start_pieces
-    pieces = []
+    pieces = {}
     positions = color == "white" ? [['a',1],['b',1],['c',1],['d',1],['e',1],['f',1],['g',1],['h',1],['a',2],['b',2],['c',2],['d',2],['e',2],['f',2],['g',2],['h',2]] :
     [['a',8],['b',8],['c',8],['d',8],['e',8],['f',8],['g',8],['h',8],['a',7],['b',7],['c',7],['d',7],['e',7],['f',7],['g',7],['h',7]]
     
-    pieces.push(Rook.new(color, positions[0]))
-    pieces.push(Knight.new(color, positions[1]))
-    pieces.push(Bishop.new(color, positions[2]))
+    pieces['rook']    = [Rook.new(color, positions[0]), Rook.new(color, positions[7])]
+    pieces['knight']  = [Knight.new(color, positions[1]), Knight.new(color, positions[6])]
+    pieces["bishop"]  = [Bishop.new(color, positions[2]), Bishop.new(color, positions[5])]
     if color == "white"
-      pieces.push(Queen.new(color, positions[3]))
-      pieces.push(King.new(color, positions[4]))
+      pieces["queen"] = [Queen.new(color, positions[3])]
+      pieces["king"]  = [King.new(color, positions[4])]
     else
-      pieces.push(King.new(color, positions[3]))
-      pieces.push(Queen.new(color, positions[4]))
+      pieces["queen"] = [Queen.new(color, positions[4])]
+      pieces["king"]  = [King.new(color, positions[3])]
     end
-    pieces.push(Bishop.new(color, positions[5]))
-    pieces.push(Knight.new(color, positions[6]))
-    pieces.push(Rook.new(color, positions[7]))
+    pieces["pawn"] = []
     for i in 8..15
-      pieces.push(Pawn.new(color, positions[i]))
+      pieces["pawn"].push(Pawn.new(color, positions[i]))
     end
     pieces
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,10 +6,10 @@ describe Player do
   end
 
   describe "#initialize" do
-    it "creates an array of starting pieces storing them in active_pieces" do
-      expect(white_player.active_pieces[0]).to be_kind_of(Rook)
+    it "creates an array of white starting pieces storing them in active_pieces" do
+      expect(white_player.active_pieces["rook"][0]).to be_kind_of(Rook)
       expect(white_player.color).to eql("white")
-      expect(white_player.active_pieces[3]).to be_kind_of(Queen)
+      expect(white_player.active_pieces["queen"][0]).to be_kind_of(Queen)
     end
   end
 end
