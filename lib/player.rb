73affe5334c2ef1253be7d71c7a3d3cd09b596c4
Aaron <acelizondo11@gmail.com>
2020-10-22 21:36:54 -0500

    Added start to player class and testing file

---
+++ lib/player.rb
@@ -0,0 +1,8 @@
+class Player
+attr_reader :color, :active_pieces, :eliminated_pieces
+  def initialize(player_color)
+    @color = player_color
+    @active_pieces
+    @eliminated_pieces
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,5 @@
+describe Player do
+  let(:white_player) do
+    @white_player = Player.new("white")
+  end
+end
